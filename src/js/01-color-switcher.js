eb5d34ab86f9911fd3778badaef5712ebfaf4158
Oleksandr Yakob <yakoboleksandr@gmail.com>
2022-01-10 17:45:53 +0200

    completed task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const bodyRef = document.body;
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+btnStart.addEventListener('click', onClickBtnStart);
+
+function onClickBtnStart() {
+  const changedColorId = setInterval(() => {
+    onClickBtnStart(getRandomHexColor);
+  }, 1000);
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  btnStop.removeAttribute('disabled');
+  btnStart.setAttribute('disabled', true);
+  btnStop.addEventListener('click', () => {
+    clearInterval(changedColorId);
+    btnStart.removeAttribute('disabled');
+    btnStop.setAttribute('disabled', true);
+  });
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}

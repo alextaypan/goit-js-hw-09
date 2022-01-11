8ed7ab45cf51d08ad9d9fde47b7b01013ed55dc3
Oleksandr Yakob <yakoboleksandr@gmail.com>
2022-01-11 19:18:44 +0200

    adhust task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,11 +7,6 @@ const refs = {
   amountRef: document.querySelector('input[name=amount]'),
 };
 
-// const form = document.querySelector('.form');
-// const delayFirst = document.querySelector('input[name=delay]');
-// const delayStep = document.querySelector('input[name=step]');
-// const amounts = document.querySelector('input[name=amount]');
-
 refs.submitRef.addEventListener('submit', event => {
   event.preventDefault();
 
@@ -49,27 +44,3 @@ refs.submitRef.addEventListener('submit', event => {
       });
   }
 });
-
-// function createPromise(position, delay) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       const shouldResolve = Math.random() > 0.3;
-//       if (shouldResolve) {
-//         resolve({ position, delay });
-//       } else {
-//         reject({ position, delay });
-//       }
-//     }, delay);
-//   });
-// }
-
-// refs.submitBtn.addEventListener('submit', () => {
-//   if (refs.amountRef.target.value === createPromise())
-//     createPromise(2, 1500)
-//       .then(({ position, delay }) => {
-//         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//       })
-//       .catch(({ position, delay }) => {
-//         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//       });
-// });
